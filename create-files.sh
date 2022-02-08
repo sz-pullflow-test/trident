c0f84456d429e895497c5e47708e49b472698f29
Syeda Zainab <zainab@gradience.io>
2022-02-08 09:34:49 +0400

    new file test-0.txt

--- create-files.sh
+++ create-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 testDirName = "test-dir-1"
-mkdir ${testDirName}
+mkdir test-dir-1
 for (( i=0; i<25; i++ ))
     do
-        touch ${testDirName}/test-${i}.txt
+        touch test-dir-1/test-${i}.txt
         git add -A && git commit -m "new file test-${i}.txt"
     done
