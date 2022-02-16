a190de4b6a267a559ab1254276f5e565e20d7266
Syeda Zainab <zainab@gradience.io>
2022-02-16 19:24:02 +0400

    new file sz-eight-point-oh-t2/test-90.txt

--- create-files.sh
+++ create-files.sh
@@ -4,5 +4,5 @@ mkdir $1
 for (( i=$2; i<($2+25); i++ ))
     do
         touch $1/test-${i}.txt
-        git add -A && git commit -m "new file test-${i}.txt"
+        git add -A && git commit -m "new file $1/test-${i}.txt"
     done
